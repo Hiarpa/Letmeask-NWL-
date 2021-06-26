b4a7b646242b4c09c12481e5d1acb85af6e92a50
Hiarpa Netto <hiarpanetto@gmail.com>
2021-06-26 12:18:21 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+//import { createContext,useState, useEffect } from 'react';
+//import { auth, firebase } from './services/firebase';
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { Room } from './pages/Room';
+
+import {AuthContextProvider} from './contexts/AuthContext';
+import { AdminRoom } from './pages/AdminRoom';
+
+
+function App() {
+  
+  return (
+  <>
+    <BrowserRouter>
+    <AuthContextProvider>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" exact component={NewRoom} />
+        <Route path="/rooms/:id" component={Room} />
+        <Route path="/admin/rooms/:id" component={AdminRoom}/>
+      </Switch>
+    </AuthContextProvider>
+    </BrowserRouter>
+  </>
+);
+}
+
+export default App;
